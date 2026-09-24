d5cc61b5a6e372870744d09af71df989ba96d855
Jonathan-Goetz <jonathan.goetz@bluewin.ch>
2026-09-24 22:08:42 +0200

    build import for word lists

--- data_clean.r
+++ data_clean.r
@@ -1,7 +1,18 @@
 library(stringr)
-library(tidy)
+library(tidyverse)
 
 all_words <- read.csv("word_lists/english_words.csv")
 
 five_letter_words <- all_words %>% 
   filter(str_length(word) == 5)
+
+frequent_words <- five_letter_words %>% 
+  filter(freq >= 1550)
+
+five_letter_words %>% 
+  select(word) %>% 
+  write_csv("word_lists/valid_guesses.csv")
+
+frequent_words %>% 
+  select(word) %>% 
+  write_csv("word_lists/valid_solutions.csv")
--- wordle_sketch.py
+++ wordle_sketch.py
@@ -1,23 +1,35 @@
 import random
+import csv
 
-words = ["apple", "adieu", "plane", "sight", "minty", "slack"]
+with open('word_lists/valid_guesses.csv', mode='r') as file:
+    reader1 = csv.DictReader(file)
+    legal_words = []
+    for row in reader1:
+        legal_words.append(row['word'])
 
-index = random.randint(0,len(words)-1)
+with open('word_lists/valid_solutions.csv', mode='r') as file:
+    reader2 = csv.DictReader(file)
+    legal_solutions = []
+    for row in reader2:
+        legal_solutions.append(row['word'])
 
-solution = words[index]
+
+index = random.randint(0,len(legal_solutions)-1)
+
+solution = legal_solutions[index]
 
 counter = 0
 
 guess = "00000"
 
-correct_letters = ["_", "_", "_", "_" ,"_"]
+correct_letters = ["_"]*5
 
 while guess != solution and counter < 6:
     correct_letters = ["_", "_", "_", "_" ,"_"]
     guess = input("Guess a word with five letters: ").lower()
     if len(guess) != 5:
         print("Your word has the wrong length!")
-    elif guess.lower() not in words:
+    elif guess.lower() not in legal_words:
         print("Your word is not in the list!")
     else:
         for i in range(0, len(solution)):
